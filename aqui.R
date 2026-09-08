7d36376c09de80a350a45b5132d845244c446225
Alvarez-Iglesias <0107579S@universityofgalway.ie>
2026-09-08 16:53:29 +0100

    Initial commit

---
+++ aqui.R
@@ -0,0 +1,12 @@
+#install.packages('jmvtools', repos=c('https://repo.jamovi.org', 'https://cran.r-project.org'))
+options(jamovi_home="C:\\Program Files\\jamovi 2.6.44.0")
+
+#jmvtools::addAnalysis(name='scatterplot', title='Scatterplot..')
+#jmvtools::addAnalysis(name='boxplot', title='Boxplot')
+#jmvtools::addAnalysis(name='piechart', title='Pie Chart')
+#jmvtools::addAnalysis(name='basicstatistics', title='Basic Statistics')
+#jmvtools::addAnalysis(name='tables', title='Tables')
+#jmvtools::addAnalysis(name='onemean', title='One-Sample t')
+
+
+jmvtools::install()
